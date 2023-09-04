ff9d328b63513a34f1dfd35b6c4e94b1db0037d6
ersin <sariiersinn13@gmail.com>
2023-09-04 13:25:22 +0300

    updated sec-grp

--- tf-files/output.tf
+++ tf-files/output.tf
@@ -2,5 +2,5 @@ output "load-balancer-dns" {
   value = "http://${aws_alb.app-load-balancer.dns_name}"
 }
 output "domain-name" {
-    value = "https://phobebook.${var.domain_name}"
+    value = "https://phonebook.${var.domain_name}"
 }
--- tf-files/security-grps.tf
+++ tf-files/security-grps.tf
@@ -9,19 +9,15 @@ resource "aws_security_group" "ec2-sec-grp" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        security_groups = [aws_security_group.load-balancer-sec-grp.id]
+    dynamic "ingress" {
+        for_each = var.port-number
+        content {
+            from_port = ingress.value
+            to_port = ingress.value
+            protocol = "tcp"
+            security_groups = [aws_security_group.load-balancer-sec-grp.id]
+        }
     }
-
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        security_groups = [aws_security_group.load-balancer-sec-grp.id]
-    }    
 
     egress {
         from_port = 0
@@ -35,18 +31,14 @@ resource "aws_security_group" "load-balancer-sec-grp" {
     name = "load-balancer-sec-grp"
     vpc_id = module.my-vpc.vpc_id
 
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+    dynamic "ingress" {
+        for_each = var.port-number
+        content {
+            from_port = ingress.value
+            to_port = ingress.value
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
     }
 
     egress {
